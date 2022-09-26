355c34f0db40cd5030901f11a06cbeb0f35e1201
binbin <binbin5474@gmail.com>
2022-09-26 13:54:26 +0800

    resume

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+import viteMd from "vite-plugin-markdown";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react(), viteMd.plugin({ mode: viteMd.Mode.HTML })],
+});
